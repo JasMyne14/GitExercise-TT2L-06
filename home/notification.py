6c032e47aa2e5d82d7a1354ed3acb3d83abc1d0c
aniqahz <aniqahazhar45@gmail.com>
2024-04-21 15:15:57 +0800

    adding notification function

--- home/notification.py
+++ home/notification.py
@@ -1,4 +1,42 @@
-from flask import Blueprint
+from flask import Flask, Blueprint
+from flask_socketio import SocketIO, emit
 
 notification = Blueprint('notification',__name__)
 
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+socketio = SocketIO(app)
+
+# Sample data (replace with database integration)
+users = {
+    'user1': {'username': 'user1', 'notifications': []},
+    'user2': {'username': 'user2', 'notifications': []}
+}
+
+@socketio.on('follow')
+def handle_follow(data):
+    follower = data['follower']
+    followed = data['followed']
+    message = f'{follower} started following you.'
+    users[followed]['notifications'].append(message)
+    emit('notification', {'message': message}, room=followed)
+
+@socketio.on('like')
+def handle_like(data):
+    liker = data['liker']
+    post_owner = data['post_owner']
+    message = f'{liker} liked your post.'
+    users[post_owner]['notifications'].append(message)
+    emit('notification', {'message': message}, room=post_owner)
+
+@socketio.on('comment')
+def handle_comment(data):
+    commenter = data['commenter']
+    post_owner = data['post_owner']
+    message = f'{commenter} commented on your post.'
+    users[post_owner]['notifications'].append(message)
+    emit('notification', {'message': message}, room=post_owner)
+
+if __name__ == '__main__':
+    socketio.run(app)
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+
 
 views = Blueprint('views',__name__)
 
@@ -7,10 +8,14 @@ def home():
     posts = [
         {'user': 'John', 'title': 'First Post', 'content': 'Hello, this is my first post!'},
         {'user': 'Jane', 'title': 'Second Post', 'content': 'Another post here!'},
-        {'user': 'Jas', 'title': 'Third Post', 'content': 'Meow is meowing!'}
+        {'user3': 'Jas', 'title': 'Third Post', 'content': 'Meow is meowing!'}
     ]
     return render_template('home.html', name='Home', posts=posts)
 
 @views.route('/notification')
 def notification():
-    return render_template('notification.html', name='Notification')
+    users = {
+        'user1': {'username': 'user1', 'notifications': []},
+        'user2': {'username': 'user2', 'notifications': []}
+    }    
+    return render_template('notification.html', notification='Notification', users=users)
