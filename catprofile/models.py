7d644b7d2144d1c079c88e31a44ad69499203ce0
JasMyne14 <jasmyne.kth@gmail.com>
2024-04-22 04:54:45 +0800

    Defining variables needed in registering cats

---
+++ catprofile/models.py
@@ -0,0 +1,12 @@
+class Cat_Profile:
+    def __init__(self, name, photo, breed, age, gender, owner_name, owner_email, contacts, special_needs, about_me):
+        self.name = name
+        self.photo = photo
+        self.breed = breed
+        self.age = age
+        self.gender = gender
+        self.owner_name = owner_name
+        self.owner_email = owner_email
+        self.contacts = contacts
+        self.special_needs = special_needs
+        self.about_me = about_me
