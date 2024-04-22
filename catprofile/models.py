bfb7dfc35cbeb50af33227a40c0e37fc04389a7c
JasMyne14 <jasmyne.kth@gmail.com>
2024-04-22 16:16:39 +0800

    Create html page for cat register

--- catprofile/models.py
+++ catprofile/models.py
@@ -1,12 +1,12 @@
 class Cat_Profile:
-    def __init__(self, name, photo, breed, age, gender, owner_name, owner_email, contacts, special_needs, about_me):
-        self.name = name
+    def __init__(self, name, photo, breed, age, gender, owner_name, owner_email, owner_contact, special_needs, about_me):
+        self.name = cat_name
         self.photo = photo
         self.breed = breed
         self.age = age
         self.gender = gender
         self.owner_name = owner_name
         self.owner_email = owner_email
-        self.contacts = contacts
+        self.owner_contact = owner_contact
         self.special_needs = special_needs
         self.about_me = about_me
