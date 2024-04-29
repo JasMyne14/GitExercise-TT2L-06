a694e5c47514f4aab6979ea9239432d3dc4be2d7
JasMyne14 <jasmyne.kth@gmail.com>
2024-04-29 22:55:25 +0800

    updated files

---
+++ models.py
@@ -0,0 +1,12 @@
+class Cat_Profile:
+    def __init__(self, name, photo, breed, age, gender, owner_name, owner_email, owner_contact, special_needs, about_me):
+        self.name = cat_name
+        self.photo = photo
+        self.breed = breed
+        self.age = age
+        self.gender = gender
+        self.owner_name = owner_name
+        self.owner_email = owner_email
+        self.owner_contact = owner_contact
+        self.special_needs = special_needs
+        self.about_me = about_me
