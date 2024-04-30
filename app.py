0fe132565d92bd37851143cb540ffc2ac60c4044
nuraleezdania <nuraleezdania05@gmail.com>
2024-05-01 00:08:32 +0800

    updated in app.py file

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+#Add database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+#Secret key
+app.config['SECRET_KEY'] = "find your treasure"
 # Add this line to configure static file serving
 app.static_folder = 'static'
 
+#Initialize the database
+db = SQLAlchemy(app)
+
+
+#Create model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+
+    #Create A String
+    def __repr__(self):
+        return 'Name %r>' % self.name
 @app.route('/')
 def index():
     return render_template("register.html")
@@ -23,3 +41,4 @@ def register():
 
 if __name__ == '__main__':
     app.run(debug=True)
+ 
