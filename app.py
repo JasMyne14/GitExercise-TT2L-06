47815dd1b4fe4ee2a66f5bad6f80372e04c5187b
nuraleezdania <nuraleezdania05@gmail.com>
2024-04-30 23:23:57 +0800

    already link css with py file

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 
-app = Flask('__name__')
+app = Flask(__name__)
 
+# Add this line to configure static file serving
+app.static_folder = 'static'
 
 @app.route('/')
 def index():
     return render_template("register.html")
 
-@app.route('/')
-@app.route('/login', methods =['GET', 'POST'])
-def login (): 
+@app.route('/login', methods=['GET', 'POST'])
+def login(): 
     if request.method == 'POST':
-
         pass
     return render_template('login.html')
 
-
-@app.route('/register', methods =['GET','POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-
         pass 
-
     return render_template('register.html')
 
 if __name__ == '__main__':
