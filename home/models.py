ef0f8aae578474afcb444aa523f5cfc48f8c2eee
nuraleezdania <nuraleezdania05@gmail.com>
2024-05-12 16:04:36 +0800

    edit code in models.py to add database for post and user

--- home/models.py
+++ home/models.py
@@ -4,10 +4,25 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime 
 from flask import current_app
 from home import db 
+from forms import RegistrationForm, LoginForm
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(255), nullable =False)
     content = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+    
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='deafult.jpg')
+    posts = db.relationship('Post', backref='author', lazy=True)
 
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
