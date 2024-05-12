9338853e6dcf0f008f949385780a090203583329
nuraleezdania <nuraleezdania05@gmail.com>
2024-05-12 16:21:11 +0800

    editted code database in models.py file

--- home/models.py
+++ home/models.py
@@ -4,14 +4,15 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime 
 from flask import current_app
 from home import db 
-from forms import RegistrationForm, LoginForm
+from .forms import RegistrationForm, LoginForm
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(255), nullable =False)
     content = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
     
@@ -20,7 +21,10 @@ class User(db.Model):
     fullname = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(20), unique=True, nullable=False)
     username = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    password1 = db.Column(db.String(60), nullable=False)
+    password2 = db.Column(db.String(60), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phonenumber = db.Column(db.String(20), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='deafult.jpg')
     posts = db.relationship('Post', backref='author', lazy=True)
 
