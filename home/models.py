3803a97a5808563918011e0abcc4935b1cea97fa
Jas3 <jasmyne.kth@gmail.com>
2024-05-20 01:46:40 +0800

    Deleted owner details section from cat profile, deleted catprofile folder

--- home/models.py
+++ home/models.py
@@ -42,9 +42,6 @@ class RegisterCat(db.Model):
     cat_vaccine = db.Column(db.String(10), nullable=False)
     cat_special_needs = db.Column(db.String(100), nullable=False)
     cat_about_me = db.Column(db.String(200), nullable=False)
-    owner_name = db.Column(db.String(100), nullable=False)
-    owner_email = db.Column(db.String(100), nullable=False)
-    owner_contact = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return f"RegisterCat('{self.cat_name}','{self.cat_photo}', '{self.cat_age}','{self.cat_breed}','{self.cat_gender}','{self.cat_neutered}','{self.cat_vaccine}','{self.cat_special_needs}','{self.cat_about_me}','{self.owner_name}','{self.owner_email}', '{self.owner_contact}')"
+        return f"RegisterCat('{self.cat_name}','{self.cat_photo}', '{self.cat_age}','{self.cat_breed}','{self.cat_gender}','{self.cat_neutered}','{self.cat_vaccine}','{self.cat_special_needs}','{self.cat_about_me}')"
