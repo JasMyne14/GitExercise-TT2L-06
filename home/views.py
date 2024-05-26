cd908d1a5c4bf1fef1ad806bee3da4fd898f1182
nuraleezdania <nuraleezdania05@gmail.com>
2024-05-26 18:07:14 +0800

    merge with main

--- home/views.py
+++ home/views.py
@@ -186,20 +186,6 @@ def profile_page():
     formcat = Cat.query.all()
     return render_template('catprofile.html', formcat=formcat)
 
-def save_picture(form_picture):
-    random_hex = secrets.token_hex(8)
-    _, f_ext = os.path.splitext(form_picture.filename)
-    picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', 'picture_fn')
-    form_picture.save(picture_path)
-
-    output_size = (125, 125)
-    i = Image.open(form_picture)
-    i.thumbnail(output_size)
-    i.save(picture_path)
-
-    return picture_fn
-
 @views.route('/user', methods=['GET','POST'])
 def user():
     return render_template('user.html')
