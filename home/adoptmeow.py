8b22f09c0dce7a2eebd698fff06192a2547a1cb0
Jas3 <jasmyne.kth@gmail.com>
2024-06-03 00:20:32 +0800

    add: remove from adoption feature

--- home/adoptmeow.py
+++ home/adoptmeow.py
@@ -42,3 +42,15 @@ def adopt_cat(cat_id):
 
     flash(f'Congratulations! You have adopted {cat.cat_name}.', 'success')
     return redirect(url_for('views.catprofile'))
+
+@adoptmeow.route('/remove_adopt/<int:cat_id>', methods=['POST'])
+@login_required
+def remove_adopt_cat(cat_id):
+    cat = Cat.query.get_or_404(cat_id)
+    if cat.user_id == current_user.id:
+        cat.available_for_adoption = False
+        db.session.commit()
+        flash('Your cat has been removed from adoption.', 'success')
+    else:
+        flash('You can only remove your own cat from adoption.', 'error')
+    return redirect(url_for('views.catprofile'))
